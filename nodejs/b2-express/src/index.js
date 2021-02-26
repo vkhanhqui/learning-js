fcc3b32fcb99033fe87cdf5e04414e4b2027f144
Khanh Qui <vkhanhqui@gmail.com>
2021-02-26 12:43:27 +0700

    integrate github api

--- nodejs/b2-express/src/index.js
+++ nodejs/b2-express/src/index.js
@@ -6,6 +6,9 @@ const db = require('./config/database')
 const route = require('./routes')
 const app = express()
 const port = 8080
+//github
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit();
 
 //connect to database
 db.connect()
@@ -21,6 +24,23 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources', 'views'))
 
 route(app);
+
+app.get('/users/:username', function (req, res) {
+    var username = req.params.username
+    var page = req.query.page
+    octokit.repos
+        .listForUser({
+            username: username,
+            type: "all",
+            per_page: "10",
+            page: page,
+            sort: "full_name"
+        })
+        .then(({ data }) => {
+            // handle data
+            res.status(200).json(data)
+        })
+})
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
